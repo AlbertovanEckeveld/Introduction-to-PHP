ee5b98011964c2210f431f96765ad92d1f0e7cbc
Alberto van Eckeveld <eckeveld@outlook.com>
2022-06-10 14:40:44 +0200

    Update

---
+++ 9. Math/index.php
@@ -0,0 +1,42 @@
+EXAMPLE 1:
+
+<?php
+echo(pi()); // returns 3.1415926535898
+?> 
+
+EXAMPLE 2:
+
+<?php
+echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+?> 
+
+EXAMPLE 3:
+
+<?php
+echo(abs(-6.7));  // returns 6.7
+?> 
+
+EXAMPLE 4:
+
+<?php
+echo(sqrt(64));  // returns 8
+?> 
+
+EXAMPLE 5:
+
+<?php
+echo(round(0.60));  // returns 1
+echo(round(0.49));  // returns 0
+?> 
+
+EXAMPLE 6:
+
+<?php
+echo(rand());
+?> 
+
+<?php
+echo(rand(10, 100));
+?> 
+
